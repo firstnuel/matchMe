b043d62414faf5ec46101680fae1db5f4e8fddc9
firstnuel <99360864+firstnuel@users.noreply.github.com>
2025-08-17 12:10:16 +0300

    Minor change

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -6,6 +6,10 @@ import (
 	"match-me/api"
 	"match-me/config"
 	"match-me/internal/repositories"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -29,4 +33,37 @@ func main() {
 	// Initialize and start HTTP server
 	srv := api.NewHTTPServer(client, cfg)
 	handleServerLifecycle(srv)
+}
+
+// handleServerLifecycle manages the lifecycle of the HTTP server
+func handleServerLifecycle(srv *http.Server) {
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	serverErr := make(chan error, 1)
+	go func() {
+		log.Printf("HTTP server listening on addr %s", srv.Addr)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			serverErr <- err
+		}
+	}()
+
+	select {
+	case err := <-serverErr:
+		log.Fatalf("Server error: %v", err)
+
+	case <-stop:
+		log.Println("Shutdown signal received. Shutting down server...")
+
+		shutdownCtx, cancel := context.WithTimeout(context.Background(), ShutdownTimeout)
+		defer cancel()
+
+		if err := srv.Shutdown(shutdownCtx); err != nil {
+			log.Fatalf("HTTP server shutdown error: %v", err)
+		} else {
+			log.Printf("HTTP server shutdown cleanly")
+		}
+	}
+
+	log.Printf("Graceful shutdown complete.")
 }
